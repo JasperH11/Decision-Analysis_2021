2a84eb961f5a14a15ae35a01c50134411468af51
Jasper Hackling <jasper.hackling@gmail.com>
2021-05-18 16:22:18 +0200

    Only suggested changes to provided code

---
+++ Seminar-06_Introduction-to-decisionSupport-package.R
@@ -0,0 +1,23 @@
+# Load required package
+library(decisionSupport)
+
+# Import example data
+example_decision_inputs <- read.csv("example_decision_inputs.csv")
+
+# Modeling function 
+example_decision_model <- function(x, varnames)
+{
+  profit <- benefits-costs
+  
+  final_profits <- profit + additional_benefits
+  
+  return(final_profits)
+  
+}
+
+
+mcSimulation(estimate = as.estimate(example_decision_inputs),
+             model_function = example_decision_model,
+             numberOfModelRuns = 700,
+             functionSyntax = "plainNames")
+
